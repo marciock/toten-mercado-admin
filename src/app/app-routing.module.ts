de383af1e1539128d8cfe639b7f8ca2752d567a0
marciock <marciockalves@gmail.com>
2021-04-19 16:24:07 -0300

    form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {DashAdminComponent} from './dash-admin/dash-admin.component';
+import {AddMarcasComponent} from './ng-marcas/add-marcas/add-marcas.component';
 
 const routes: Routes = [
-  {path:'',component:DashAdminComponent}
+  {path:'',component:DashAdminComponent},
+  {path:'add_marcas',component:AddMarcasComponent}
 ];
 
 @NgModule({
